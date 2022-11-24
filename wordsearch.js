596e7439779f68a70a13f44c032bb117d0745391
Ebele <ebele.omanukwue@telus.com>
2022-11-23 20:56:00 -0700

    word search complted with Tai Dalisle

--- wordsearch.js
+++ wordsearch.js
@@ -2,7 +2,22 @@ const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
-    }
+      }
+
+      const VerticalArr = [];
+      for (const array of letters) {
+        for (const alpha in array) {
+          if (!VerticalArr[alpha]) VerticalArr[alpha] = [];
+          VerticalArr[alpha].push(array[alpha]);
+        }
+      }
+      const VerticalJoin = VerticalArr.map(ls => ls.join(''))
+      for (l of VerticalJoin) {
+        if (l.includes(word)) return true
+      }
+    return false
 }
 
 module.exports = wordSearch
+
+
